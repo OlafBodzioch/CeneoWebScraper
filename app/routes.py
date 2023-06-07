f915af8b10c8f4a10e99bb9aad3e6d2e24e92533
OlafBodzioch <226356@student.uek.krakow.pl>
2023-06-07 12:59:38 +0200

    2023-06-07 OB

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,23 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "!hello world!"
+    return render_template('index.html')
 
-@app.route("/name", defaults={'name': 'anonim'})
-@app.route("/name/<name>")
-def name(name):
-    return f"!hello {name}!"
+@app.route("/lista")
+def lista():
+    return render_template('lista.html')
+
+@app.route("/product")
+def product():
+    return render_template('product.html')
+
+@app.route("/ekstrakcja")
+def ekstrakcja():
+    return render_template('ekstrakcja.html')
+
+@app.route("/autor")
+def autor():
+    return render_template('autor.html')
