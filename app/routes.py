a372024b54d3c88cea6533ca1b18a5e29b5be2a5
OlafBodzioch <226356@student.uek.krakow.pl>
2023-06-14 22:03:10 +0200

    2023-06-14 OB Ekstrakcja fixes

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,9 @@ from app.utils import get_element, selectors
 import requests
 import json
 import os
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 from bs4 import BeautifulSoup
 
 @app.route("/")
@@ -59,7 +62,48 @@ def ekstrakcja():
         with open(f"./app/static/opinions/{product_code}.json", "w", encoding="UTF-8") as jf:
             json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
 
-    # !!!!!!!!!!!!!!!!!!!!!Dodać tutaj gdzieś OPINIONS PD READ, SCORE,  SHAPE, MAP i inne do wypisywania rzeczy chyba z analysera!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        opinions = pd.read_json(json.dumps(all_opinions,ensure_ascii=False))
+        opinions.score = opinions.score.map(lambda x: float(x.split("/")[0].replace(",",".")))
+        stats = {
+            "opinions_count": opinions.shape[0],
+            "pros_count": int(opinions.pros.map(bool).sum()),
+            "cons_count": int(opinions.cons.map(bool).sum()),
+            "avg_score": opinions.score.mean().round(2)
+        }
+        score = opinions.score.value_counts().reindex(list(np.arange(0,5.5,0.5)), fill_value = 0)
+        score.plot.bar(color="hotpink")
+        plt.xticks(rotation=0)
+        plt.title("Histogram ocen")
+        plt.xlabel("Liczba gwiazdek")
+        plt.ylabel("Liczba opinii")
+        plt.ylim(0,max(score.values)+1.5)
+        for index, value in enumerate(score):
+            plt.text(index, value+0.5, str(value), ha="center")
+        try:
+            os.mkdir("./app/static/plots")
+        except FileExistsError:
+            pass
+        plt.savefig(f"./app/static/plots/{product_code}_score.png")
+        plt.close()
+        recommendation = opinions["recommendation"].value_counts(dropna = False).reindex(["Nie polecam", "Polecam", np.nan])
+        print(recommendation)
+        recommendation.plot.pie(
+            label="", 
+            autopct="%1.1f%%",
+            labels = ["Nie polecam", "Polecam", "Nie mam zdania"],
+            colors = ["crimson", "forestgreen", "gray"]
+        )
+        plt.legend(bbox_to_anchor=(1.0,1.0))
+        plt.savefig(f"./app/static/plots/{product_code}_recommendation.png")
+        plt.close()
+        stats['score'] = score.to_dict()
+        stats['recommendation'] = recommendation.to_dict()
+        try:
+            os.mkdir("./app/static/stats")
+        except FileExistsError:
+            pass
+        with open(f"./app/static/stats/{product_code}.json", "w", encoding="UTF-8") as jf:
+            json.dump(stats, jf, indent=4,ensure_ascii=False)
 
         return redirect(url_for('product', product_code=product_code))
     return render_template('ekstrakcja.html')
